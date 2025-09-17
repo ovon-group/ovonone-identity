36bb694e81002feaa1a119c5168013510593f6bf
Chris Reid <chris@cloudest.co.uk>
2025-09-17 12:01:26 +0100

    Add w2w link

--- resources/views/filament/widgets/app-launcher.blade.php
+++ resources/views/filament/widgets/app-launcher.blade.php
@@ -1,5 +1,12 @@
 <x-filament-widgets::widget>
     <x-filament::section>
-        <x-filament::link href="https://dealer.protegoautocare.test">Protego</x-filament::link>
+        <ul>
+            <li>
+                <x-filament::link href="https://dealer.protegoautocare.test">Protego</x-filament::link>
+            </li>
+            <li>
+                <x-filament::link href="https://wheel2web.test">Wheel2Web</x-filament::link>
+            </li>
+        </ul>
     </x-filament::section>
 </x-filament-widgets::widget>
